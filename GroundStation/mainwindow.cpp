db8604b697816a0a12ecd34688db2e4de7ab94a4
Thar0l <gusarov.aa@gmail.com>
2016-10-16 13:35:27 +0300

    Added functionality for connecting to serial ports.

--- GroundStation/mainwindow.cpp
+++ GroundStation/mainwindow.cpp
@@ -3,12 +3,76 @@
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    ui(new Ui::MainWindow),
+    serialPort(new QSerialPort)
 {
     ui->setupUi(this);
+    connected = false;
+    serialPort->setBaudRate(QSerialPort::Baud9600);
+    serialPort->setDataBits(QSerialPort::Data8);
+    serialPort->setParity(QSerialPort::NoParity);
+    serialPort->setStopBits(QSerialPort::OneStop);
 }
+
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete serialPort;
 }
+
+
+void MainWindow::on_btnFind_clicked()
+{
+    ui->cbPorts->clear();
+    QList<QSerialPortInfo> serialPorts = QSerialPortInfo::availablePorts();
+    if (serialPorts.length() == 0)
+    {
+        ui->teLog->appendHtml("<font color=red>No port found.</font>");
+    }
+    else
+    {
+        ui->teLog->appendHtml("<font color=green>Found ports:</font>");
+    }
+    for (QSerialPortInfo portInfo : serialPorts)
+    {
+        ui->cbPorts->addItem(portInfo.portName());
+        ui->teLog->appendHtml("<font color=blue>" + portInfo.portName() + "</font>");
+    }
+}
+
+
+void MainWindow::on_btnConnect_clicked()
+{
+    if (connected)
+    {
+        serialPort->close();
+        connected = false;
+        ui->teLog->appendHtml("<font color=red>Disconnected.</font>");
+        ui->btnConnect->setText("Connect");
+    }
+    else
+    {
+        if (ui->cbPorts->currentText().length() == 0)
+        {
+            ui->teLog->appendHtml("<font color=red>No port selected.</font>");
+        }
+        else
+        {
+            serialPort->setPortName(ui->cbPorts->currentText());
+            serialPort->open(QIODevice::ReadWrite);
+            if (serialPort->isOpen())
+            {
+                connected = true;
+                ui->teLog->appendHtml("<font color=green>Connected to " + serialPort->portName() + ".</font>");
+                ui->btnConnect->setText("Disconnect");
+            }
+            else
+            {
+                ui->teLog->appendHtml("<font color=red>Error connecting to " + serialPort->portName() + ".</font>");
+            }
+        }
+    }
+}
+
+
--- GroundStation/mainwindow.h
+++ GroundStation/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +17,16 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_btnFind_clicked();
+
+    void on_btnConnect_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QSerialPort *serialPort;
+    bool connected;
+
 };
 
 #endif // MAINWINDOW_H
